b407691da40e023d1a4d4c0cb53a069a25e9bcce
kimjunsoo <rlawnstn01023@naver.com>
2021-10-13 15:51:08 +0900

    백신 총합

--- COVID-19_vaccine_Total/ViewController.swift
+++ COVID-19_vaccine_Total/ViewController.swift
@@ -1,19 +1,73 @@
-//
-//  ViewController.swift
-//  COVID-19_vaccine_Total
-//
-//  Created by 준수김 on 2021/10/13.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, XMLParserDelegate {
 
+    var xmlParser = XMLParser()
+
+    var currentElement = ""
+    var items = [[String : String]]()
+    var item = [String : String]()
+    var vaccinetpcd = ""
+    var vaccinefirstCnt = ""
+    var vaccinesecondCnt = ""
+    var vaccinethirdCnt = ""
+
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        requestVaccineSidoInfo()
     }
 
+    func requestVaccineSidoInfo() {
+        let url = "https://nip.kdca.go.kr/irgd/cov19stats.do?list=all"
+        guard let xmlParser = XMLParser(contentsOf: URL(string: url)!) else { return }
+        xmlParser.delegate = self
+        xmlParser.parse()
+    }
+    public func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?,attributes attributeDict: [String : String] = [:]) {
+        currentElement = elementName
+        if elementName == "item" {
+            item = [String : String]()
+            vaccinetpcd = ""
+            vaccinefirstCnt = ""
+            vaccinesecondCnt = ""
+            vaccinethirdCnt = ""
+            //print("didStartElement")
+        }
+    }
 
+    public func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "item" {
+            
+            item["tpcd"] = vaccinetpcd
+            item["firstCnt"] = vaccinefirstCnt
+            item["secondCnt"] = vaccinesecondCnt
+            item["thridCnt"] = vaccinethirdCnt
+            items.append(item)
+
+            //print("didEndElement")
+        }
+
+    }
+
+    public func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if currentElement == "tpcd" {
+            vaccinetpcd = string
+            if vaccinetpcd != "" {
+            print("시도명: \(vaccinetpcd)")
+            }
+        } else if (currentElement == "firstCnt") {
+            vaccinefirstCnt = string
+            print("1차 백신 총 접종자 수: \(vaccinefirstCnt)")
+
+        } else if (currentElement == "secondCnt") {
+            vaccinesecondCnt = string
+            print("2차 백신 총 접종자 수: \(vaccinesecondCnt)")
+
+        } else if (currentElement == "thirdCnt") {
+            vaccinethirdCnt = string
+            print("3차 백신 총 접종자 수: \(vaccinethirdCnt)")
+
+        }
+    }
 }
-
