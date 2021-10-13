8de4ae473463f553aeeae51018597ac1c1e4341d
kimjunsoo <rlawnstn01023@naver.com>
2021-10-13 15:41:06 +0900

    Initial Commit

---
+++ COVID-19_vaccine_Total/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID-19_vaccine_Total
+//
+//  Created by 준수김 on 2021/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
